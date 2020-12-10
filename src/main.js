8eed9b1a9760ccfe3f38c47ec6e9a3c3f26a7325
Paulo Wuéliton <paulo.wueliton@hotmail.com>
2020-12-09 22:18:14 -0300

    feat: added form control script

--- src/main.js
+++ src/main.js
@@ -1,2 +1,4 @@
 import "./assets/css/style.css";
 import formControl from "./modules/formControl";
+
+formControl();
--- src/modules/formControl.js
+++ src/modules/formControl.js
@@ -1,7 +1,68 @@
-import passGenerator from "./modules/generatePassword";
+import passGenerator from "./generatePassword";
 
-const formControl = () => {
-  console.log("teste");
+const pass = document.querySelector(".pass");
+const lengthPass = document.querySelector(".length-pass");
+const chkUpperCase = document.querySelector(".chkUpperCase");
+const chkLowerCase = document.querySelector(".chkLowerCase");
+const chkNumbers = document.querySelector(".chkNumbers");
+const chkSimbols = document.querySelector(".chkSimbols");
+const progressBar = document.querySelector(".pass-security");
+const refresh = document.querySelector(".refresh");
+
+export default () => {
+  pass.innerHTML = generate();
+
+  document.addEventListener("change", (el) => {
+    if (el.target.type === "checkbox") {
+      pass.innerHTML = generate();
+    }
+  });
+
+  lengthPass.addEventListener("change", (e) => {
+    e.preventDefault();
+    pass.innerHTML = generate();
+  });
+
+  refresh.addEventListener("click", (e) => {
+    e.preventDefault();
+    refresh.classList.add("active");
+    setTimeout(() => {
+      refresh.classList.remove("active");
+    }, 500);
+    pass.innerHTML = generate();
+  });
 };
 
-export default formControl;
+function generate() {
+  const pass = passGenerator(
+    lengthPass.value,
+    chkUpperCase.checked,
+    chkLowerCase.checked,
+    chkNumbers.checked,
+    chkSimbols.checked
+  );
+
+  progressStatus(lengthPass.value);
+
+  return pass;
+}
+
+function progressStatus(value) {
+  if (value < 10) {
+    let percent = (value / 100) * 1000;
+    progressBar.style.width = percent + "%";
+
+    if (value < 4) {
+      progressBar.classList.add("danger");
+      progressBar.classList.remove("alert");
+    } else if (value < 8) {
+      progressBar.classList.add("alert");
+      progressBar.classList.remove("danger");
+    } else {
+      progressBar.classList.remove("alert");
+      progressBar.classList.remove("danger");
+    }
+  } else {
+    progressBar.style.width = "100%";
+  }
+}
